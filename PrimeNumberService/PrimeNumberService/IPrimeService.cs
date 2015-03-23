f908e28de8e0bb6579fb53b5d5b44d1ee4c96e4f
badgerfan23 <lyyien@yahoo.com>
2015-03-23 00:39:09 -0500

    Commit Stable version 1 of the code where the web service do return well the list of the first 1000 prime numbers to the client website.

---
+++ PrimeNumberService/PrimeNumberService/IPrimeService.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PrimeNumberService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IPrimeService
+    {
+        [OperationContract]
+        [
+             WebInvoke(Method = "*",
+             ResponseFormat = WebMessageFormat.Json,
+             BodyStyle = WebMessageBodyStyle.Wrapped,
+             UriTemplate = "ShowThousandPrime/")
+         ]
+
+        string ShowThousandPrime();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class ListThousandPrimes
+    {
+        [DataMember]
+        private StringBuilder PrimeNumber = new StringBuilder();        
+
+        public void setPrimeNumber(string num){
+            PrimeNumber.Append(num);
+        }
+
+        public string getPrimeNumber() {
+            return PrimeNumber.ToString();
+        }
+
+        public void removeLast() {
+            PrimeNumber.Length --;
+            PrimeNumber.Length--;
+        }
+    }
+}
---
+++ PrimeNumberService/PrimeNumberService/PrimeService.svc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace PrimeNumberService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class PrimeService : IPrimeService
+    {
+        private const int LimitPrimeNumber = 1000;
+
+        public Boolean IsPrime(int num)
+        {
+            int factor = num / 2;
+
+            for (int j = 2; j <= factor; j++)
+            {
+                if ((num % j) == 0) return false;
+            }
+
+            return true;
+        }
+
+        public string ShowThousandPrime()
+        {
+            ListThousandPrimes answer = new ListThousandPrimes();
+
+            int number = 0;
+
+            int indexPrime = 0;
+
+            while (indexPrime <= LimitPrimeNumber + 1)
+            {
+
+                if (IsPrime(number))
+                {
+                    if (number >= 2) answer.setPrimeNumber(number.ToString() + ", ");
+
+                    indexPrime++;
+                }
+
+                number++;
+            }
+
+            answer.removeLast();
+
+            return answer.getPrimeNumber();
+        }
+    }
+}
